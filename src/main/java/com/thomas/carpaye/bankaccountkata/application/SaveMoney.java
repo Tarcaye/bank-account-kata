87462d524b289e713fbef972bc02a9e97f98959b
Thomas Carpaye <thomas.carpaye@gmail.com>
2019-06-13 00:24:24 +0200

    US 1 - must_save_money_on_bank_account_for_bank_client_when_deposit_money_multiple_times red

---
+++ src/main/java/com/thomas/carpaye/bankaccountkata/application/SaveMoney.java
@@ -0,0 +1,10 @@
+package com.thomas.carpaye.bankaccountkata.application;
+
+public class SaveMoney {
+
+
+
+    public static void save(String account, int money) {
+
+    }
+}
--- src/test/java/com/thomas/carpaye/bankaccountkata/application/SaveMoneyTest.java
+++ src/test/java/com/thomas/carpaye/bankaccountkata/application/SaveMoneyTest.java
@@ -1,5 +1,6 @@
-package com.thomas.carpaye.bankaccountkata;
+package com.thomas.carpaye.bankaccountkata.application;
 
+import com.thomas.carpaye.bankaccountkata.application.SaveMoney;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
@@ -16,17 +17,28 @@ public class SaveMoneyTest {
         int money = 12000;
 
         // Act
-        save(accountId, money);
+        SaveMoney.save(accountId, money);
 
         // Assert
         Assertions.assertThat(balance(accountId).getValue()).isEqualTo(money);
     }
 
-    private Balance balance(String accountId) {
-        return new Balance(12000);
+    @Test
+    public void must_save_money_on_bank_account_for_bank_client_when_deposit_money_multiple_times() {
+        // Arrange
+        String accountId = "1234567890123";
+        int money = 12000;
+
+        // Act
+        SaveMoney.save(accountId, money);
+        SaveMoney.save(accountId, money);
+
+        // Assert
+        Assertions.assertThat(balance(accountId).getValue()).isEqualTo(money*2);
     }
 
-    private void save(String account, int money) {
+    private Balance balance(String accountId) {
+        return new Balance(12000);
     }
 
     public class Balance {
