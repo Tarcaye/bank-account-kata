9e972e03ba0d3666d25c2bcc9cc44b550620b149
Thomas Carpaye <thomas.carpaye@gmail.com>
2021-06-23 01:00:51 +0200

    Refactor test name : the_balance_of_a_bank_acount_after_a_single_deposit_is_the_amount_of_the_deposit

--- src/test/java/bank/SaveMoneyTest.java
+++ src/test/java/bank/SaveMoneyTest.java
@@ -7,15 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SaveMoneyTest {
 
     @Test
-    void save_money() {
-        Account account = new Account(new Client());
-
-        account.deposit(new Amount(100));
-
-        assertThat(account.getBalance()).isEqualTo(new Amount(100));
-    }
-
-    @Test
     void the_balance_of_a_new_bank_acount_is_zero() {
         Account account = new Account(new Client());
 
@@ -24,6 +15,15 @@ public class SaveMoneyTest {
         assertThat(balance).isEqualTo(new Amount(0));
     }
 
+
+    @Test
+    void the_balance_of_a_bank_acount_after_a_single_deposit_is_the_amount_of_the_deposit() {
+        Account account = new Account(new Client());
+
+        account.deposit(new Amount(100));
+
+        assertThat(account.getBalance()).isEqualTo(new Amount(100));
+    }
 
 
 }
