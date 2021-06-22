9f56febe938582a266e133d1cc65b97428c2d019
Thomas Carpaye <thomas.carpaye@gmail.com>
2021-06-23 00:58:34 +0200

    the_balance_of_a_new_bank_acount_is_zero

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,13 +1,18 @@
 package bank;
 
 class Account {
+
+    private Amount balance;
+
     public Account(Client client) {
+        balance = new Amount(0);
     }
 
     public void deposit(Amount amount) {
+        balance = amount;
     }
 
     public Amount getBalance() {
-        return new Amount(100);
+        return balance;
     }
 }
