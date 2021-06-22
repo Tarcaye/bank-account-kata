fd9319970ee47c7a23d1453a0bec5434e9de67f4
Thomas Carpaye <thomas.carpaye@gmail.com>
2021-06-23 00:57:14 +0200

    the_balance_of_a_new_bank_acount_is_zero

--- src/test/java/bank/SaveMoneyTest.java
+++ src/test/java/bank/SaveMoneyTest.java
@@ -15,6 +15,15 @@ public class SaveMoneyTest {
         assertThat(account.getBalance()).isEqualTo(new Amount(100));
     }
 
+    @Test
+    void the_balance_of_a_new_bank_acount_is_zero() {
+        Account account = new Account(new Client());
+
+        Amount balance = account.getBalance();
+
+        assertThat(balance).isEqualTo(new Amount(0));
+    }
+
 
 
 }
