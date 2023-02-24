0912bde657a789766dd31573cd1d1cdbc191f894
Matthieu BONNOT <mbonnot@ippon.fr>
2023-02-24 10:59:17 +0100

    should_return_2_when_deposit_of_two

--- src/test/java/bank/TotoTest.java
+++ src/test/java/bank/TotoTest.java
@@ -5,14 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class TotoTest {
 
+    private final Account account = new Account();
+
     @Test
     void should_return_1_when_deposit_of_one() {
-        Assertions.assertThat(deposit(1)).isEqualTo(1);
+        Assertions.assertThat(account.deposit(1)).isEqualTo(1);
+
     }
 
+    @Test
+    void should_return_2_when_deposit_of_two() {
+        Assertions.assertThat(account.deposit(2)).isEqualTo(2);
 
+    }
 
-    private int deposit(int i) {
-        return 1;
+    public static class Account {
+        public Account() {
+
+        }
+
+        private int deposit(int i) {
+            return i;
+        }
     }
 }
